4d57d5f29bb4b47a2e4d75d8c4eafbbe4fda24c7
p1995 <pj1311995@gmail.com>
2020-08-17 12:37:37 +0530

    Updates

---
+++ precache-manifest.cda485999a2af6bed6c7878909228a8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4ceb71a287614ed61fced979902701",
+    "url": "/Rich-Text-Editor/index.html"
+  },
+  {
+    "revision": "296808964d3e01e97443",
+    "url": "/Rich-Text-Editor/static/css/2.5646f6cd.chunk.css"
+  },
+  {
+    "revision": "d2692512748fc896b81d",
+    "url": "/Rich-Text-Editor/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "296808964d3e01e97443",
+    "url": "/Rich-Text-Editor/static/js/2.b53a0a04.chunk.js"
+  },
+  {
+    "revision": "68c5efac6a31c95f86fa48d1ef4a1c8f",
+    "url": "/Rich-Text-Editor/static/js/2.b53a0a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2692512748fc896b81d",
+    "url": "/Rich-Text-Editor/static/js/main.b97607c1.chunk.js"
+  },
+  {
+    "revision": "aeae04559a400bb50589",
+    "url": "/Rich-Text-Editor/static/js/runtime-main.511ee409.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Rich-Text-Editor/static/media/logo.5d5d9eef.svg"
+  }
+]);
